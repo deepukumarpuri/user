ffd6033b879465a6aae6cb61fa172f2c2698b58e
madboy482 <73926361+madboy482@users.noreply.github.com>
2021-09-16 14:52:21 +0530

    Modified

--- ULTRA/plugins/arts.py
+++ ULTRA/plugins/arts.py
@@ -1,5 +1,4 @@
-from ULTRA.utils import admin_cmd, edit_or_reply, sudo_cmd
-from ULTRA import CMD_HELP, ALIVE_NAME
+
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "υℓтяα χ"
 
@@ -8,8 +7,7 @@ USERID = bot.uid
 mention = f"[{DEFAULTUSER}](tg://user?id={USERID})"
 
 
-@bot.on(admin_cmd(pattern="carry (.*)"))
-@bot.on(sudo_cmd(pattern="carry (.*)", allow_sudo=True))
+
 async def legendx22(carry):
     name = carry.pattern_match.group(1)
     await edit_or_reply(
@@ -18,8 +16,6 @@ async def legendx22(carry):
     )
 
 
-@bot.on(admin_cmd(pattern="ded (.*)"))
-@bot.on(sudo_cmd(pattern="ded (.*)", allow_sudo=True))
 async def legendx22(ded):
     name = ded.pattern_match.group(1)
     await edit_or_reply(
@@ -41,8 +37,7 @@ async def legendx22(ded):
     )
 
 
-@bot.on(admin_cmd(pattern="killer (.*)"))
-@bot.on(sudo_cmd(pattern="killer (.*)", allow_sudo=True))
+
 async def legendx22(killer):
     name = killer.pattern_match.group(1)
     await edit_or_reply(
@@ -71,8 +66,7 @@ A = (
 )
 
 
-@bot.on(admin_cmd(pattern="sthink (.*)"))
-@bot.on(sudo_cmd(pattern="sthink (.*)", allow_sudo=True))
+
 async def legendx22(think):
     name = think.pattern_match.group(1)
     B = (
@@ -96,8 +90,7 @@ async def legendx22(think):
     await edit_or_reply(think, B)
 
 
-@bot.on(admin_cmd(pattern="sfrog (.*)"))
-@bot.on(sudo_cmd(pattern="sfrog (.*)", allow_sudo=True))
+
 async def legendx22(frogsay):
     name = frogsay.pattern_match.group(1)
     C = (
@@ -121,8 +114,7 @@ async def legendx22(frogsay):
     await edit_or_reply(frogsay, C)
 
 
-@bot.on(admin_cmd(pattern="sdead (.*)"))
-@bot.on(sudo_cmd(pattern="sdead (.*)", allow_sudo=True))
+
 async def legendx22(deadfrog):
     name = deadfrog.pattern_match.group(1)
     D = (
@@ -146,8 +138,6 @@ async def legendx22(deadfrog):
     await edit_or_reply(deadfrog, D)
 
 
-@bot.on(admin_cmd(pattern="strump (.*)"))
-@bot.on(sudo_cmd(pattern="strump (.*)", allow_sudo=True))
 async def legendx22(trumpsay):
     name = trumpsay.pattern_match.group(1)
     E = (
@@ -171,8 +161,6 @@ async def legendx22(trumpsay):
     await edit_or_reply(trumpsay, E)
 
 
-@bot.on(admin_cmd(pattern="schina (.*)"))
-@bot.on(sudo_cmd(pattern="schina (.*)", allow_sudo=True))
 async def legendx22(ckmkb):
     name = ckmkb.pattern_match.group(1)
     F = (
@@ -197,14 +185,12 @@ async def legendx22(ckmkb):
     await edit_or_reply(ckmkb, F)
 
 
-@bot.on(admin_cmd(pattern=r"sshit$"))
-@bot.on(sudo_cmd(pattern="sshit$", allow_sudo=True))
+
 async def legendx22(shit):
     await edit_or_reply(shit, A)
 
 
-@bot.on(admin_cmd(pattern=r"elove$"))
-@bot.on(sudo_cmd(pattern="elove$", allow_sudo=True))
+
 async def kakashi(event):
     await edit_or_reply(
         event,
@@ -526,141 +512,87 @@ D = (
     "╱╰╯╰╯╰╯╰┻╯╰╯╱╰╯╰╯╰╯╰┻╯╰┻━╯╱╰╯\n"
 )
     
-@bot.on(admin_cmd(pattern=r"monster$"))
-@bot.on(sudo_cmd(pattern="monster$", allow_sudo=True))
+
 async def bluedevilmonster(monster):
     await edit_or_reply(monster, G)
 
-@bot.on(admin_cmd(pattern=r"ooof$"))
-@bot.on(sudo_cmd(pattern="ooof$", allow_sudo=True))
+
 async def bluedevilbigoof(bigoof):
     await edit_or_reply(bigoof, A)
 
-@bot.on(admin_cmd(pattern=r"fook$"))
-@bot.on(sudo_cmd(pattern="fook$", allow_sudo=True))
+
 async def bluedevilfook(fook):
     await edit_or_reply(fook, B)
 
-@bot.on(admin_cmd(pattern=r"wdf$"))
-@bot.on(sudo_cmd(pattern="wdf$", allow_sudo=True))
+
 async def bluedevilwhadfuk(whadfuk):
     await edit_or_reply(whadfuk, C)
     
-@bot.on(admin_cmd(pattern=r"wdyw$"))
-@bot.on(sudo_cmd(pattern="wdyw$", allow_sudo=True))
+
 async def bluedevilwhadayawant(whadayawant):
     await edit_or_reply(whadayawant, D)
     
-@bot.on(admin_cmd(pattern=r"pig$"))
-@bot.on(sudo_cmd(pattern="pig$", allow_sudo=True))
+
 async def bluedevilpig(pig):
     await edit_or_reply(pig, H)
 
 
-@bot.on(admin_cmd(pattern=r"gun$"))
-@bot.on(sudo_cmd(pattern="gun$", allow_sudo=True))
+
 async def bluedevilgun(gun):
     await edit_or_reply(gun, I)
 
 
-@bot.on(admin_cmd(pattern=r"dog$"))
-@bot.on(sudo_cmd(pattern="dog$", allow_sudo=True))
+
 async def bluedevildog(dog):
     await edit_or_reply(dog, J)
 
 
-@bot.on(admin_cmd(pattern=r"hello$"))
-@bot.on(sudo_cmd(pattern="hello$", allow_sudo=True))
+
 async def bluedevilhello(hello):
     await edit_or_reply(hello, K)
 
 
-@bot.on(admin_cmd(pattern=r"hmf$"))
-@bot.on(sudo_cmd(pattern="hmf$", allow_sudo=True))
 async def bluedevilhmf(hmf):
     await edit_or_reply(hmf, L)
 
 
-@bot.on(admin_cmd(pattern=r"couple$"))
-@bot.on(sudo_cmd(pattern="couple$", allow_sudo=True))
 async def bluedevilcouple(couple):
     await edit_or_reply(couple, M)
 
 
-@bot.on(admin_cmd(pattern=r"sup$"))
-@bot.on(sudo_cmd(pattern="sup$", allow_sudo=True))
+
 async def bluedevilsupreme(supreme):
     await edit_or_reply(supreme, N)
 
 
-@bot.on(admin_cmd(pattern=r"india$"))
-@bot.on(sudo_cmd(pattern="india$", allow_sudo=True))
+
 async def bluedevilindia(india):
     await edit_or_reply(india, O)
 
 
-@bot.on(admin_cmd(pattern=r"wc$"))
-@bot.on(sudo_cmd(pattern="wc$", allow_sudo=True))
+
 async def bluedevilwelcome(welcome):
     await edit_or_reply(welcome, P)
 
 
-@bot.on(admin_cmd(pattern=r"snk$"))
-@bot.on(sudo_cmd(pattern="snk$", allow_sudo=True))
+
 async def bluedevilsnake(snake):
     await edit_or_reply(snake, Q)
 
 
-@bot.on(admin_cmd(pattern=r"bye$"))
-@bot.on(sudo_cmd(pattern="bye$", allow_sudo=True))
+
 async def bluedevilbye(bye):
     await edit_or_reply(bye, R)
 
 
-@bot.on(admin_cmd(pattern=r"shitos$"))
-@bot.on(sudo_cmd(pattern="shitos$", allow_sudo=True))
+
 async def bluedevilshitos(shitos):
     await edit_or_reply(shitos, S)
 
 
-@bot.on(admin_cmd(pattern=r"dislike$"))
-@bot.on(sudo_cmd(pattern="dislike$", allow_sudo=True))
+
 async def bluedevildislike(dislike):
     await edit_or_reply(dislike, T)
 
 
 
-CMD_HELP.update(
-    {
-        "arts": "__**PLUGIN NAME :** Arts__\
-      \n\n** CMD ~>** `.elove` | `.monster` | `.pig` | `.gun` | `.dog` | `.hello` | `.hmf` | `.couple` | `.sup` | `.india` | `.wc` | `.snk` | `.bye` | `.shitos` | `.dislike`\
-      \n\n**USAGE   ~>  **These are arts,use & see\
-      \n\n** CMD ~>** `.carry` <your text>\
-      \n**USAGE   ~>  **Send your text with carry's art.\
-      \n\n** CMD ~>** `.killer` <text>\
-      \n**USAGE   ~>  **Be a warrior & kill enimes.\
-      \n\n** CMD ~>** `.ded` <text>\
-      \n**USAGE   ~>  **Hang yourself.\
-      \n\n** CMD ~>** `.sthink` <your text>\
-      \n**USAGE   ~>  **Send your text which tou thinking about.\
-      \n\n** CMD ~>** `.sfrog` <your text>\
-      \n**USAGE   ~>  **Send your text with frog's art.\
-      \n\n** CMD ~>** `.sdead` <your text>\
-      \n**USAGE   ~>  **Send your text with dead forg's art.\
-      \n\n** CMD ~>** `.strump` <your text>\
-      \n**USAGE   ~>  **Send your text with trump's art.\
-      \n\n** CMD ~>** `.china` <your text>\
-      \n**USAGE   ~>  **Send your text with china's art.\
-      \n\n** CMD ~>** `.sshit` \
-      \n**USAGE   ~>  **Ahhh shit...here we go again..\
-      \n\n** CMD ~>** `.ooof` \
-      \n**USAGE   ~>  **Big oooooooooooooooooooooooooooooooooof..\
-      \n\n** CMD ~>** `.fook` \
-      \n**USAGE   ~>  **Wtf kiddo?..\
-      \n\n** CMD ~>** `.wdf` \
-      \n**USAGE   ~>  **Wha D Fuk??..\
-      \n\n** CMD ~>** `.wdyw` \
-      \n**USAGE   ~>  **Wha du ya want Kiddo??..\
-"
-    }
-)
